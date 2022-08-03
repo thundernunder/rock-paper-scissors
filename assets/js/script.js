b2b41685b94160eea645a0396171b853d8c59908
Nunder <nathannunderwood@gmail.com>
2022-08-02 20:24:59 -0700

    Game built. Want to figure out how to display computer choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,41 @@
 var result = document.querySelector('#result');
+var wins = 0;
+var losses = 0;
+var ties = 0;
+var showWins = document.querySelector('#wins');
+var showLosses = document.querySelector('#losses');
+var showTies = document.querySelector('#ties');
+var submitAnswer = document.querySelector('#submit');
 
 
-function getComputerChoice() {
-    var comChoices = ["rock", "paper", "scissors"];
-}
+submitAnswer.addEventListener('click', function getComputerChoice() {
+    var choices = ["rock", "paper", "scissors"];
+
+    var compChoice = choices[Math.floor(choices.length * Math.random())];
+
+    var userChoice = document.getElementById('user-input')
+    .value
+    .toLowerCase();
+    
+    console.log(compChoice);
+
+    if (userChoice === 'rock' && compChoice === 'scissors' ||
+        userChoice === 'scissors' && compChoice === 'paper' ||
+        userChoice === 'paper' && compChoice === 'rock') {
+            alert('You win!');
+            wins++;
+            showWins.textContent = "Wins " + wins;
+        } else if (userChoice === 'rock' && compChoice === 'rock' ||
+        userChoice === 'scissors' && compChoice === 'scissors' ||
+        userChoice === 'paper' && compChoice === 'paper') {
+            alert('Tie!');
+            ties++;
+            showTies.textContent = "Ties " + ties;
+        } else {
+            alert('You lose!');
+            losses++;
+            showLosses.textContent = "Losses " + losses;
+        }
+});
+
+
